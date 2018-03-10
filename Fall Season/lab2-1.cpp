8fd2af79718f037ffcf9263c433fd8406bb68191
handoso <35907967+handoso@users.noreply.github.com>
2018-03-10 19:48:14 +0800

    Add files via upload

---
+++ Fall Season/lab2-1.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main(){
+	int a,b,c,e,f,a1,b1,c1,a2,b2,c2,d2,e2,f2,s,s1,s2,s3,s4,s5,z1,z2,z3,z4;
+	/*printf("types: ");
+	scanf("%d %d", &a, &b); c = a > b; printf(">: %d\n", c);
+	c = a < b; printf("<: %d\n", c);
+	c = a <= b; printf("<=: %d\n", c);
+	c = a >= b; printf(">=: %d\n", c);
+	c = a != b; printf("!=: %d\n", c);
+	c = a == b; printf("==: %d\n\n", c); printf("2 jishix: ");
+	scanf("%d %d",&e,&f); 
+	c = (e<f)*e+(e>=f)*f; printf("%d ",c);
+	c = (e>f)*e+(e<=f)*f; printf("%d\n\n",c);
+	a1 = 6; c1 = 8; b1 = 10; printf("3 daxi daalgawar: a:%d b:%d c:%d\n",a1,b1,c1);
+	c1 < b1 && a1 < c1 && printf("bolj ");
+	(a1 % 3 == 0 || c1 % 5 == 3) && printf("bna.\n\n");printf("4 daxi daalgawar \n");
+	/*a2 = 1; b2 = 0; c2 = 4; d2 = 0; e2 = 3; f2 = 1;
+	a2 && printf("o") && c2; 
+	c2 && d2 || printf("x");
+	(d2 || e2) && f2 || printf("x");
+	a2 + e2 == 1 && printf("x");
+	a2 && printf("o");
+	e2 && d2 || printf("o");*/. 
+ 	/*printf("5 daxi daalgawar: \n"); scanf("%d %d %d",&s,&s1,&s2);
+ 	s3 = (s>s1)?(s>s2)?s:s2:(s1>s2)?s1:s2; printf("ix: %d\n", s3);
+ 	s4 = (s<s1)?(s<s2)?s:s2:(s1<s2)?s1:s2; printf("baga: %d\n", s4);
+ 	s5 = (s>s1)?(s>s2)?s2:s:(s1>s2)?s2:s1; printf("dynd: %d\n", s5);
+ 	printf("6 daxi daalgawar: \n"); scanf("%d %d %d",&z2,&z3,&z4);*/
+ 	z2 == z3 || z2 == z4 || z3 == z4 && printf("adil hajuut\n");
+    printf("7 daxi daalgawar: \n"); scanf("%d", &z1);
+ 	//(z1/1000000>=1)?printf("7 orontoi"):(z1/100000>=1)?printf("6 orontoi\n"):(z1/10000>=1)?printf("5 orontoi"):(z1/1000>=1)?printf("4 orontoi"):(z1/100>=1)?printf("3 orontoi\n"):(z1/10>=1)?printf("2 orontoi\n"):printf("1 orontoi");
+	return 0;
+}
