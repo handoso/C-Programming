30db5fcd1e8e80b6d463dc2f5f3943faac65e9a7
handoso <35907967+handoso@users.noreply.github.com>
2018-05-09 13:46:07 +0800

    Add files via upload

---
+++ homework-14-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void read(int *p){
+  scanf("%d",p+2);
+}
+int main(){
+  int a[100],i;
+  int j=2;
+  for(i=0; i<5; i++){
+    read(a-j); j--;
+  }
+  for(i=0; i<5; i++){
+    printf("%d ",a[i]);
+  }
+  return 0;
+}
